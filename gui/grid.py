8a6897b243cb9d70917b45c5566970c4a532b04f
Rohit Srivastava <srivastr14@Rohits-MacBook-Air.local>
2022-10-03 21:55:37 -0400

    Cute message for Hannah

--- gui/grid.py
+++ gui/grid.py
@@ -3,8 +3,8 @@ from tkinter import *
 root = Tk()
 
 # Creating Label widget
-myLabel1 = Label(root, text='Hello World')
-myLabel2 = Label(root, text='My Name is Rohit Srivastava')
+myLabel1 = Label(root, text='Hey... Hey.. HEY...')
+myLabel2 = Label(root, text='I love you')
 
 # Remember, the grid is all relative
 myLabel1.grid(row=0, column=0)
