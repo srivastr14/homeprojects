6033beb12b18a10ad3f24a78211d8dd044d127e7
WorkRohit <srivastr14@gmail.com>
2022-12-01 14:33:54 -0500

    Found a way to ignore the word AND

--- ORNL.py
+++ ORNL.py
@@ -1,20 +1,25 @@
 import re
 import pyperclip as pc
 
-first_initial = re.compile(r"(\w)(?:\W?\w+)*\s[^,]*")
-last_name = re.compile(r"[^,]*\s(?!and)(\w+(?:\W?\w+)*)")
+def initializer():
+    first_initial = re.compile(r"(?![and])(\w)(?:\W?\w+)*\s[^,]*")
+    last_name = re.compile(r"[^,]*\s(?!and)(\w+(?:\W?\w+)*)")
 
 
-message = pc.paste()
-# message = 'Rohit Srivastava, Hannah Nelsen, Peanut Smith'
-first = list(first_initial.findall(message))
-last = list(last_name.findall(message))
+    message = pc.paste()
+    # message = 'Rohit efd Srivastava, Hannah G. Nelsen, and Peanut G. Smith'
+    first = list(first_initial.findall(message))
+    last = list(last_name.findall(message))
 
-list_names = ""
-for f,l in zip(first, last):
-    if l == last[-1]:
-        list_names += f+". "+l
-    else:
-        list_names += f+". "+l+", "
+    list_names = ""
+    for f,l in zip(first,last):
+        print(f)
+        if l == last[-1]:
+            list_names += f+". "+l
+        else:
+            list_names += f+". "+l+", "
 
-pc.copy(list_names)
+    pc.copy(list_names)
+
+if __name__ == "__main__":
+    initializer()
