73afc301dd22437829134ebd5ad7ccf71bd0f276
WorkRohit <srivastr14@gmail.com>
2022-12-06 12:58:10 -0500

    Making progress... Not sure if this is a good way

--- monthly_email.py
+++ monthly_email.py
@@ -8,17 +8,22 @@ df = df.fillna('')
 df.rename(columns={'Citation_PuSH_style___________________________________________': 'Citation'}, inplace=True)
 
 bold = "\033[1m"
+clear = '\033[0m'
 
-for y,x in df.iterrows():
+for y, x in df.iterrows():
     citation = x[0]
     if x[2] and int(x[2]) > 7:
-        print(Back.GREEN + bold + "JIF: " + str(x[2]) + Back.RESET)
+        jif = Back.GREEN + bold + "JIF: " + str(x[2]) + clear + Back.RESET
+    elif x[2] == '' or int(x[2]) < 7:
+        jif = ''
+    if x[3] != ".":
+        linked = Fore.RED + x[3] + Fore.RESET
+    print(citation, jif, linked)
 
-
-
-# Concept: Take csv and make lists and then add as paragraphs (including adding "JIF: " and "Linked: ")
-# This includes highlighting "JIF: number" in Green and red text for "linked: number", bold instrument/noninstrument, and hyperlinked doi
-# Then separate by instrument and make note of any DOI above 25 as "Notable High Impact Papers this Month:"
-# Other headers: "HFIR and SNS Instrument Publications" and "NScD Publications (including Accelerator Physics, Irradiation, Isotopes, Facility, and Staff publications)"
+# Concept: Take csv and make lists and then add as paragraphs (including adding "JIF: " and "Linked: ") This includes
+# highlighting "JIF: number" in Green and red text for "linked: number", bold instrument/noninstrument,
+# and hyperlinked doi. Then separate by instrument and make note of any DOI above 25 as "Notable High Impact Papers
+# this Month:" Other headers: "HFIR and SNS Instrument Publications" and "NScD Publications (including Accelerator
+# Physics, Irradiation, Isotopes, Facility, and Staff publications)"
 
 # Make sure to manually add two charts from https://snsapp1.sns.ornl.gov/xprod/f?p=133:19:104649382691778:::::
